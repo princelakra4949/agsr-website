1da1845feb1554d1f22e5d83ebd1ec0fd0b5f462
prince <princelakra4949@gmail.com>
2026-06-09 11:21:38 +0000

    Add global JS error catcher to surface exact crash message on screen

--- cms/src/app/layout.tsx
+++ cms/src/app/layout.tsx
@@ -8,7 +8,5 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
+      <head>
+        <script dangerouslySetInner
